6644e4823847e81e431d94392964cb08552ab16a
ilina-m <61251012+ilina-m@users.noreply.github.com>
2022-03-13 04:27:26 +0200

    Finished
    daily and escape mars not working on website

---
+++ planet_guesser/game.js
@@ -0,0 +1,276 @@
+let myX = 780, myY = 430, myW = 25, myH = 50;
+let direction = [2];
+let ironrockX = 440, ironrockY = 50;
+let fuelX = 188, fuelY = 141;
+let pickaxeX = 910, pickaxeY = 560;
+let plan1X = 500, plan1Y = 560;
+let plan2X = 1052, plan2Y = 117;
+
+let touchiron = false, gotiron = false;
+let touchfuel = false, gotfuel = false;
+let touchplan1 = false, gotplan1 = false;
+let touchplan2 = false, gotplan2 = false;
+let touchpickaxe = false, gotpickaxe = false;
+let finish = true;
+
+let fuel = 0; pick = 0, plan = 0, iron = 0;
+
+
+
+function update() {
+
+   //basic movements
+
+   direction = ["0", "0"];
+
+
+   if (isKeyPressed[87]) {
+      myY -= 2;
+      direction[1] = "up";
+   }
+
+   if (isKeyPressed[83]) {
+      myY += 2;
+      direction[1] = "down";
+   }
+
+   if (isKeyPressed[68]) {
+      myX += 2;
+      direction[2] = "right";
+   }
+
+   if (isKeyPressed[65]) {
+      myX -= 2;
+      direction[2] = "left";
+   }
+   //checking collision 
+   if (areColliding(myX, myY, myW, myH, 95, 30, 10, 160)) { 
+      if (direction[2] == "left") {
+         myX += 2;
+      }
+      if (direction[2] == "right") {
+         myX -= 2;
+      }
+      if (direction[1] == "up") {
+         myY += 2;
+      }
+      if (direction[1] == "down") {
+         myY -= 2;
+      }
+   }
+
+   if (areColliding(myX, myY, myW, myH, 95, 180, 310, 10)) {
+      if (direction[1] == "up") {
+         myY += 2;
+      }
+      if (direction[1] == "down") {
+         myY -= 2;
+      }
+      if (direction[2] == "left") {
+         myX += 2;
+      }
+      if (direction[2] == "right") {
+         myX -= 2;
+      }
+   }
+
+   if (areColliding(myX, myY, myW, myH, 95, 30, 310, 10)) {
+      if (direction[1] == "up") {
+         myY += 2;
+      }
+      if (direction[1] == "down") {
+         myY -= 2;
+      }
+      if (direction[2] == "left") {
+         myX += 2;
+      }
+      if (direction[2] == "right") {
+         myX -= 2;
+      }
+   }
+
+   if (areColliding(myX, myY, myW, myH, 395, 30, 10, 75)) {
+      if (direction[1] == "up") {
+         myY += 2;
+      }
+      if (direction[1] == "down") {
+         myY -= 2;
+      }
+      if (direction[2] == "left") {
+         myX += 2;
+      }
+      if (direction[2] == "right") {
+         myX -= 2;
+      }
+   }
+
+
+   //collision between items and char
+   if (areColliding(myX, myY, myW, myH, plan1X, plan1Y, 20, 20)) {
+      touchplan1 = true;
+   } else touchplan1 = false
+
+   if (areColliding(myX, myY, myW, myH, plan2X, plan2Y, 20, 20)) {
+      touchplan2 = true;
+   } else touchplan2 = false
+
+   if (areColliding(myX, myY, myW, myH, fuelX, fuelY, 20, 20)) {
+      touchfuel = true;
+   } else touchfuel = false;
+
+   if (areColliding(myX, myY, myW, myH, pickaxeX, pickaxeY, 20, 20)) {
+      touchpickaxe = true;
+   } else touchpickaxe = false;
+
+   if (areColliding(myX, myY, myW, myH, ironrockX, ironrockY, 20, 20)) {
+      touchiron = true;
+   } else touchiron = false
+
+
+   //text
+   if (gotpickaxe == true) {
+      if (plan == 2) {
+         alert("I can use this to break the rock! :D");
+      }
+      gotpickaxe = false;
+   }
+
+   if (gotiron == true) {
+      if (pick == 0) {
+         alert("Oh, it's just a rock...");
+      } else if (pick == 1) {
+         alert("I can fix the rocket with this iron!");
+      }
+      gotiron = false;
+   }
+
+   if (gotfuel == true) {
+      alert("What's fuel doing here? Oh well, it's useful");
+      gotfuel = false;
+   }
+
+   if (gotplan1 == true || gotplan2 == true) {
+      if (plan == 1) {
+         alert("A part of the plan!");
+         gotplan1 = false;
+         gotplan2 = false;
+      }
+      if (plan == 2) {
+         alert("I have the whole plan now! I should check out the rocket for instruments");
+         gotplan1 = false;
+         gotplan2 = false;
+      }
+   }
+   if (fuel == 1 && iron == 1 && plan == 2 && finish == true) {
+      alert("I have everything I need! I can go home now.");
+      finish = false;
+   }
+
+
+   //BORDERS
+
+   if (myX >= 1600) {
+      myX -= 2;
+   }
+   if (myX <= 0) {
+      myX += 2;
+   }
+   if (myY >= 800) {
+      myY -= 2;
+   }
+   if (myY <= 0) {
+      myY += 2;
+   }
+
+
+}
+
+function draw() {
+
+
+   drawImage(mars, 0, 0, 1640, 940);
+   drawImage(room, 95, 40, 300, 140);
+   drawImage(astronaut, myX, myY, myW, myH);
+
+
+   //1housedrawing
+   context.fillStyle = 'black';
+   context.fillRect(95, 30, 10, 160); //left
+   context.fillStyle = 'black';
+   context.fillRect(95, 180, 310, 10); //bottom
+   context.fillStyle = 'black';
+   context.fillRect(95, 30, 310, 10); // top
+   context.fillStyle = 'black';
+   context.fillRect(395, 30, 10, 75); //right
+
+   //parts!
+   drawImage(rockwithiron, ironrockX, ironrockY, 70, 50);
+   
+   
+   
+   var image = new Image(display="none");
+ 
+   // Add onload event handler
+   image.onload = function () {
+      drawImage(fuel, fuelX, fuelY, 25, 30);
+   };
+ 
+   // Set the source url of the image
+   image.src = "images\fuel.png";
+   
+   
+   
+   
+   drawImage(backgroundfuel, 220, 155, 40, 25);
+   drawImage(fakepart[3], pickaxeX, pickaxeY, 50, 50)
+   drawImage(rocket, 850, 480, 200, 220);
+   drawImage(fakepart[2], plan1X, plan1Y, 30, 30);
+   drawImage(fakepart[2], plan2X, plan2Y, 30, 30);
+
+}
+
+function keyup(key) {
+   console.log("Pressed", key);
+   if (key == 69) {
+      if (touchpickaxe == true) {
+         gotpickaxe = true;
+         if (plan == 2) {
+            pickaxeX = -1000000;
+            pickaxeY = -1000000;
+            pick++;
+         }
+      }
+      if (touchiron == true) {
+         gotiron = true;
+         if (pick == 1) {
+            ironrockX = -1000000;
+            ironrockY = -1000000;
+            iron++;
+         }
+      }
+      if (touchfuel == true) {
+         gotfuel = true;
+         fuelX = -1000000;
+         fuelY = -1000000;
+         fuel++;
+      }
+      if (touchplan1 == true) {
+         gotplan1 = true;
+         plan1X = -1000000;
+         plan1Y = -1000000;
+         plan++;
+      }
+      if (touchplan2 == true) {
+         gotplan2 = true;
+         plan2X = -100000;
+         plan2Y = -100000;
+         plan++;
+      }
+   }
+}
+
+function mouseup() {
+   console.log("Mouse clicked at", mouseX, mouseY);
+}
+
+
--- planet_guesser/main.py
+++ planet_guesser/main.py
@@ -1,106 +1,131 @@
 import pathlib
+import wikipedia
+import numpy as np
+import os
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
 
-AUTOTUNE = tf.data.AUTOTUNE
-file_path = '/home/george/Python/planet_guesser/dataset'
-file_path = pathlib.Path(file_path)
+def test(file_path):
+    AUTOTUNE = tf.data.AUTOTUNE
+    #file_path = 'C:\Users\ilina\AppData\Local\Programs\Python\Python310\Scripts\minigames\planet_guesser\dataset'
+    file_path = pathlib.Path(file_path)
 
-BATCH_SIZE = 8
-IMG_HEIGHT = 180
-IMG_WIDTH = 180
+    BATCH_SIZE = 8
+    IMG_HEIGHT = 180
+    IMG_WIDTH = 180
 
-train_ds = tf.keras.utils.image_dataset_from_directory(
-  file_path,
-  validation_split=0.2,  # 20% for validation
-  subset="training",
-  seed=123,
-  image_size=(IMG_HEIGHT, IMG_WIDTH),
-  batch_size=BATCH_SIZE)
+    train_ds = tf.keras.utils.image_dataset_from_directory(
+    file_path,
+    validation_split=0.2,  # 20% for validation
+    subset="training",
+    seed=123,
+    image_size=(IMG_HEIGHT, IMG_WIDTH),
+    batch_size=BATCH_SIZE)
 
-val_ds = tf.keras.utils.image_dataset_from_directory(
-  file_path,
-  validation_split=0.2,  # 20% for validation
-  subset="validation",
-  seed=123,
-  image_size=(IMG_HEIGHT, IMG_WIDTH),
-  batch_size=BATCH_SIZE)
+    val_ds = tf.keras.utils.image_dataset_from_directory(
+    file_path,
+    validation_split=0.2,  # 20% for validation
+    subset="validation",
+    seed=123,
+    image_size=(IMG_HEIGHT, IMG_WIDTH),
+    batch_size=BATCH_SIZE)
 
-class_names = train_ds.class_names
+    class_names = train_ds.class_names
 
-train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
-val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
-normalization_layer = layers.Rescaling(1./255)
-normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
-image_batch, labels_batch = next(iter(normalized_ds))
-first_image = image_batch[0]
+    train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+    val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+    normalization_layer = layers.Rescaling(1./255)
+    normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
+    image_batch, labels_batch = next(iter(normalized_ds))
+    first_image = image_batch[0]
 
-num_classes = len(class_names)
+    num_classes = len(class_names)
 
-model = Sequential([
-  layers.Rescaling(1./255, input_shape=(IMG_HEIGHT, IMG_WIDTH, 3)),
-  layers.Conv2D(16, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D(),
-  layers.Conv2D(32, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D(),
-  layers.Conv2D(64, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D(),
-  layers.Flatten(),
-  layers.Dense(128, activation='relu'),
-  layers.Dense(num_classes)
+    model = Sequential([
+    layers.Rescaling(1./255, input_shape=(IMG_HEIGHT, IMG_WIDTH, 3)),
+    layers.Conv2D(16, 3, padding='same', activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Conv2D(32, 3, padding='same', activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Conv2D(64, 3, padding='same', activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Flatten(),
+    layers.Dense(128, activation='relu'),
+    layers.Dense(num_classes)
+    ])
+
+    model.compile(optimizer='adam',
+                loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                metrics=['accuracy'])
+
+    epochs = 10
+    history = model.fit(
+    train_ds,
+    validation_data=val_ds,
+    epochs=epochs
+    )
+
+    data_augmentation = keras.Sequential(
+    [
+        layers.RandomFlip("horizontal",
+                        input_shape=(IMG_HEIGHT, IMG_WIDTH, 3)),
+        layers.RandomRotation(0.1),
+        layers.RandomZoom(0.1),
+    ]
+    )
+
+    model = Sequential([
+    data_augmentation,
+    layers.Rescaling(1./255),
+    layers.Conv2D(16, 3, padding='same', activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Conv2D(32, 3, padding='same', activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Conv2D(64, 3, padding='same', activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Dropout(0.2),
+    layers.Flatten(),
+    layers.Dense(128, activation='relu'),
+    layers.Dense(num_classes)
 ])
 
-model.compile(optimizer='adam',
-              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-              metrics=['accuracy'])
+    model.compile(optimizer='adam',
+                loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                metrics=['accuracy'])
 
-epochs = 10
-history = model.fit(
-  train_ds,
-  validation_data=val_ds,
-  epochs=epochs
-)
+    epochs = 15
+    history = model.fit(
+    train_ds,
+    validation_data=val_ds,
+    epochs=epochs
+    )
 
-data_augmentation = keras.Sequential(
-  [
-    layers.RandomFlip("horizontal",
-                      input_shape=(IMG_HEIGHT, IMG_WIDTH, 3)),
-    layers.RandomRotation(0.1),
-    layers.RandomZoom(0.1),
-  ]
-)
+    epochs = 15
+    history = model.fit(
+    train_ds,
+    validation_data=val_ds,
+    epochs=epochs
+    )
 
-model = Sequential([
-  data_augmentation,
-  layers.Rescaling(1./255),
-  layers.Conv2D(16, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D(),
-  layers.Conv2D(32, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D(),
-  layers.Conv2D(64, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D(),
-  layers.Dropout(0.2),
-  layers.Flatten(),
-  layers.Dense(128, activation='relu'),
-  layers.Dense(num_classes)
-])
+    directory = os.listdir('C:\Users\ilina\PycharmProjects\hacktues\planet_guesser\Test')
+    file_path = f'C:\Users\ilina\PycharmProjects\hacktues\planet_guesser\Test\{directory[0]}'
+    file_path = pathlib.Path(file_path)
 
-model.compile(optimizer='adam',
-              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-              metrics=['accuracy'])
+    img = tf.keras.utils.load_img(
+        file_path, target_size=(IMG_HEIGHT, IMG_WIDTH)
+    )
+    img_array = tf.keras.utils.img_to_array(img)
+    img_array = tf.expand_dims(img_array, 0)
 
-epochs = 15
-history = model.fit(
-  train_ds,
-  validation_data=val_ds,
-  epochs=epochs
-)
+    predictions = model.predict(img_array)
+    score = tf.nn.softmax(predictions[0])
 
-epochs = 15
-history = model.fit(
-  train_ds,
-  validation_data=val_ds,
-  epochs=epochs
-)
+    planet = f"{class_names[np.argmax(score)]}"
+    accuracy = f"{round(100 * np.max(score), 2)}"
+    text = wikipedia.summary(f'{planet}(planet)', sentences=2)
+    os.remove(f'C:\Users\ilina\PycharmProjects\hacktues\planet_guesser\Test\{directory[0]}')
+    return planet
+
+test()
